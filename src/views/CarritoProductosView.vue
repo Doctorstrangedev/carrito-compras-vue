b2e405d6c8dfd440f4a705e3bf25a7b83a641d30
doctorstrangedev <paginasdpb@gmail.com>
2022-03-29 19:03:19 -0300

    aviso de carrito vacio

--- src/views/CarritoProductosView.vue
+++ src/views/CarritoProductosView.vue
@@ -19,7 +19,7 @@
 
       <tfoot>
         <tr>
-          <!-- <th>Carrito Vacio, ningún producto seleccionado</th> -->
+          <th colspan="5" v-if="Object.keys(carrito).length ===0">Carrito Vacio, Ningún producto seleccionado</th>
           <FooterTotal />
         </tr>
       </tfoot>
